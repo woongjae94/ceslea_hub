a9b2b14175d9c00beaa61010f9ca37deb22fe6fd
woong <skydnd0304@gmail.com>
2021-01-18 23:06:35 +0900

    init7
    init7

--- abr-demo_gesture/run_main_gesture_and_control.sh
+++ abr-demo_gesture/run_main_gesture_and_control.sh
@@ -3,4 +3,4 @@ export DISPLAY=:0.0
 xhost local:root
 #sudo docker run --runtime=nvidia --network="host" -v ~/Desktop/COPY_UPGRADE/abr-demo_gesture:/home/app -it --rm --name Gesture_container woongjae94/abr-demo:gesture python main_gesture_and_control.py
 
-sudo docker run --runtime=nvidia --network="host"  --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/shm:/dev/shm -v ~/Desktop/COPY_UPGRADE/abr-demo_gesture:/home/app -it --rm --name Gesture_container woongjae94/abr-demo:gesture python main_gesture_and_control.py
+sudo docker run --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=1 --network="host"  --env="DISPLAY=$DISPLAY" --env="QT_X11_NO_MITSHM=1" -v /tmp/.X11-unix:/tmp/.X11-unix:ro -v /dev/shm:/dev/shm -v ~/Desktop/COPY_UPGRADE/abr-demo_gesture:/home/app -it --rm --name Gesture_container woongjae94/abr-demo:gesture python main_gesture_and_control.py
